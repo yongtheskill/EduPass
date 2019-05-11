867fd08711a26b9d11bf42f42fa26cfa89e927f2
Yongtheskill <yongee2003@gmail.com>
2019-05-11 18:16:51 +0800

    Update wsgi.py

--- EduPass/wsgi.py
+++ EduPass/wsgi.py
@@ -1,22 +1,12 @@
+"""
+WSGI config for myproject project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
 import os
-import time
-import traceback
-import signal
-import sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EduPass.settings")
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EduPass.settings')
-
-try:
-    application = get_wsgi_application()
-    print ('WSGI without exception')
-except Exception:
-    print ('handling WSGI exception')
-    # Error loading applications
-    if 'mod_wsgi' in sys.modules:
-        traceback.print_exc()
-        os.kill(os.getpid(), signal.SIGINT)
-        time.sleep(2.5)
-
-
-
+application = get_wsgi_application()
