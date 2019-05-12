125b19adeaa69b4f2f60a740a1f2b306e4b73777
Yongtheskill <yongee2003@gmail.com>
2019-05-12 16:36:38 +0800

    Update settings.py

--- EduPass/settings.py
+++ EduPass/settings.py
@@ -133,9 +133,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
