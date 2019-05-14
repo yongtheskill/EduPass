1b7a8bb186dff621e7a3e3fe63161e2a932c57c6
Yongtheskill <yongee2003@gmail.com>
2019-05-15 01:39:28 +0800

    .

--- home/models.py
+++ home/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    StudentName = models.CharField(max_length=100)
-    ParentName = models.CharField(max_length=100)
-    Money = models.DecimalField(decimal_places=2, max_digits=10)
-    IsPaid = models.BooleanField()
+    StudentName = models.CharField(max_length=100, default='')
+    ParentName = models.CharField(max_length=100, default='')
+    Money = models.DecimalField(decimal_places=2, max_digits=10, default=0.00)
+    IsPaid = models.BooleanField(default=False)
 
--- readme.md
+++ readme.md
@@ -5,3 +5,25 @@ source /opt/python/run/venv/bin/activate <p>
 source /opt/python/current/env<p>
 cd /opt/python/current/app<p>
 python manage.py (commands)
+
+
+Canteen
+- Wanton Noodle
+- Nasi Lemak 
+- Roti Prata
+- Chicken Rice
+- Laksa
+
+Bookshop
+- Pen
+- Curve ruler
+- Calculator
+- Dictionary 
+- correction tape
+
+Café
+- Fish and Chips
+- Waffles
+- Sandwich
+- Hot Chocolate
+- Taco
